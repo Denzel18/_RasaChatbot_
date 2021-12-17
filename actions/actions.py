9f7982c24dfe3105ff4e6621123d4266995e3290
emanueleincicco <48356595+emanueleincicco@users.noreply.github.com>
2021-12-17 10:45:14 +0100

    aggiunto: nuovo ordine + bozza modifica ordine + bozza cancellazione ordine

--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,50 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from random import randint
+import csv
+import os.path
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+def random_with_N_digits(n):
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
+    return randint(range_start, range_end)
+
+
+class ActionSaveOrder(Action):
+
+    def name(self) -> Text:
+        return 'action_save_order'
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # get slots values
+        food = str(tracker.get_slot('food'))
+        time = str(tracker.get_slot('time'))
+        # create a random order ID
+        order_id = f'AA{random_with_N_digits(5)}'
+
+        # check if orders file exists
+        filename = './files/orders.csv'
+
+        if os.path.exists(filename):
+            # append if already exists
+            file = open(filename, 'a', newline='')
+        else:
+            # make a new file if not
+            file = open(filename, 'w', newline='')
+            writer = csv.writer(file)
+            writer.writerow(['Order ID', 'Time', 'Plate'])
+
+        writer = csv.writer(file)
+        writer.writerow([order_id, time, food])
+        file.close()
+        dispatcher.utter_message(text=f'Order Saved! Your order ID is {order_id}')
+
+        return []
